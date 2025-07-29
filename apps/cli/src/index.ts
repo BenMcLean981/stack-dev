01ff1b001f5e125fd8db2bad0c9df3f6b88860b1
Ben McLean <35312653+BenMcLean981@users.noreply.github.com>
2025-07-29 12:11:51 -0400

    Renamed config-package to packages.

--- apps/cli/src/index.ts
+++ apps/cli/src/index.ts
@@ -1,5 +1,5 @@
 import { Command } from 'commander'
-import { createConfigPackage } from './config-package'
+import { createConfigPackage } from './packages'
 import { createWorkspace } from './workspace'
 
 const program = new Command()
